db0d8d0f248f0bdf0f4ee3a29fd400954776d363
NicolasRitouet <nicolas@ritouet.com>
2015-02-13 13:08:26 +0100

    WIP: test deploy to Heroku

---
+++ README.md
@@ -0,0 +1,2 @@
+# deployd-heroku-demo
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/NicolasRitouet/deployd-heroku-demo)
---
+++ server.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var express = require('express');
+var http = require('http');
+
+var serverPort = process.env.PORT || 3000;
+var nodeEnv = process.env.NODE_ENV || 'development';
+
+// setup express
+var app = exports.app = express();
+var server = http.createServer(app);
+
+// Define a new route
+app.get('/hello-express', function (req, res) {
+  res.send('Hello Deployd!');
+});
+
+// setup deployd
+require('deployd').attach(server, {
+  env: nodeEnv,
+  db: {
+    connectionString: process.env.MONGOHQ_URL
+                        || process.env.MONGOLAB_URI
+                        || 'mongodb://localhost:27017/deployd'
+  }
+});
+
+// After attach, express can use server.handleRequest as middleware
+app.use(server.handleRequest);
+
+// start server
+server.listen(serverPort, function() {
+  var serverAddr = server.address().address == '0.0.0.0' ? 'localhost' : server.address().address;
+  console.log('Express & Deployd started.\n\nPlease visit http://%s:%s', serverAddr, server.address().port);
+});
